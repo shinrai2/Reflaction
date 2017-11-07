a2ee75a565f8d9edff43896044bd6306015d8a33
shinrai2 <fuck@shinrai.cc>
2017-11-07 23:56:47 +0800

    2017-11-7 23:56:42

--- reflaction/src/main/java/cc/shinrai/reflaction/ReflactTree.java
+++ reflaction/src/main/java/cc/shinrai/reflaction/ReflactTree.java
@@ -1,13 +1,67 @@
 package cc.shinrai.reflaction;
 
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Created by Shinrai on 2017/11/2 0002.
  */
 
-class ReflactTree {
+public class ReflactTree {
+    private Method _method;
+    private Object _receiver;
+    private ARGV[] _ARGV_OBJECTS;
 
+    private ReflactTree(String script) {
+        // TODO
+    }
 
     public Object exec() {
+        Object result = null;
+        Object[] objects = new Object[_ARGV_OBJECTS.length];
+        for(int i=0;i<_ARGV_OBJECTS.length;i++)
+            objects[i] = _ARGV_OBJECTS[i].get();
+        try {
+            result = _method.invoke(_receiver, objects); // exec the method
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static ReflactTree[] build(String script) {
+        // TODO
         return null;
+    }
+}
+
+/**
+ * the inner function ARGV's class.
+ */
+class ARGV {
+    private Object _object;
+    private ReflactTree _tree;
+    private boolean flag;
+
+    /**
+     * Rule:
+     * @obj set flag true.
+     * @tree set flag false;
+     */
+    public ARGV(boolean flag, Object obj, ReflactTree tree) {
+        if(flag)
+            _object = obj;
+        else
+            _tree = tree;
+    }
+
+    public Object get() {
+        if(flag)
+            return _object;
+        else
+            return _tree.exec();
     }
 }
--- reflaction/src/main/java/cc/shinrai/reflaction/Reflaction.java
+++ reflaction/src/main/java/cc/shinrai/reflaction/Reflaction.java
@@ -1,6 +1,5 @@
 package cc.shinrai.reflaction;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 
 /**
@@ -9,28 +8,31 @@ import java.util.HashMap;
 
 public class Reflaction {
     private HashMap<String, Object> __OBJECTS__;
-    private ReflactTree mReflactTree;
+    private ReflactTree[] mReflactTree;
 
     public Reflaction() {
         __OBJECTS__ = new HashMap<>();
     }
+
     public void put(String symbol, Object object) {
         __OBJECTS__.put(symbol, object);
     }
+
     public Object get(String symbol) {
-        if(symbol.equals("__SELF__"))
+        if(symbol.equals("__SELF__")) // direct to HashMap self.
             return __OBJECTS__;
         Object obj = __OBJECTS__.get(symbol);
         if(obj != null)
             return obj;
         else
-            // TODO
-        return null;
+            return null;
     }
     public void reload() {
-
+        if(mReflactTree != null && mReflactTree.length != 0)
+            for(int i=0;i<mReflactTree.length;i++)
+                mReflactTree[i].exec();
     }
-    public void reload(String script) {
-
+    public void load(String script) {
+        mReflactTree = ReflactTree.build(script);
     }
 }
